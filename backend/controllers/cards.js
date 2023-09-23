564ae8a46fb7db30eee58c0919601cea5f8c49c7
Vikosik99 <viktoria.b99@yandex.ru>
2023-09-23 13:35:33 +0300

    test13

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -7,7 +7,6 @@ const ForbiddenError = require('../errors/ForbiddenError');
 module.exports.addCard = (req, res, next) => {
   const { name, link } = req.body;
   Card.create({ name, link, owner: req.user._id })
-    .orFail()
     .populate('owner')
     .then((card) => {
       res.status(201).send(card);
